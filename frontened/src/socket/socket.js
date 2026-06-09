1c87599ecc3950d17562758c04729d55d12ef2c5
Roshani-Rai <roshaniray2626@gmail.com>
2026-06-09 10:32:16 +0530

    Socket io Integartion

--- frontened/src/socket/socket.js
+++ frontened/src/socket/socket.js
@@ -6,4 +6,8 @@ const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
+  transports: ['websocket'],   
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
