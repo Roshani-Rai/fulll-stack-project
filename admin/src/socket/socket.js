1b757827c6dacde91c1581b38146eb2d80832f4d
Roshani-Rai <roshaniray2626@gmail.com>
2026-06-09 10:31:09 +0530

    Socket io Integartion

--- admin/src/socket/socket.js
+++ admin/src/socket/socket.js
@@ -4,8 +4,10 @@ const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000'
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
-  withCredentials: true
-})
+  withCredentials: true,
+  transports: ['websocket'],   
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
 
-socket.on('connect', () => console.log('SOCKET CONNECTED ✅', socket.id))
-socket.on('connect_error', (err) => console.log('SOCKET ERROR ❌', err.message))
